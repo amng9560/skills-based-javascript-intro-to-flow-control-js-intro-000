67d6c953da085d8c545a23d6841d0bc091148944
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-27 15:19:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,34 +1,41 @@
 function basicTeenager(age) {
-  if (age >= 13 && age <=10) {
+
+  if(age >= 13 && age <= 19) {
     return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-  if(age >=13 && age <=19) {
+
+  if(age >= 13 && age <= 19) {
     return "You are a teenager!"
-} else {
+  } else {
     return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-  if (age >= 13 && age <=19) {
+
+  if(age >= 13 && age <= 19) {
     return "You are a teenager!"
-} else if (age < 12) {
+  } else if(age <= 12) {
     return "You are a kid"
-{ else (age > 19)
+  } else if(age > 19) {
     return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 
+  return age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+
   switch(age <= 19 && age >= 13) {
-    case true:
+    case true :
     return "You are a teenager"
     default:
     return "You have an age"
-
+  }
 }
